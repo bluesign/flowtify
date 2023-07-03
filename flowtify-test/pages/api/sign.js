069faced60c19db11b0d9795fb52dd2d07f5453c
Deniz Mert Edincik <deniz@edincik.com>
2023-07-03 20:59:13 +0200

    simple example

--- flowtify-test/pages/api/sign.js
+++ flowtify-test/pages/api/sign.js
@@ -10,20 +10,21 @@ const hash = (message) => {
 }
 
 const sign = (privateKey, message) => {
-  const key = ec.keyFromPrivate(Buffer.from(privateKey, "hex"))
-  const sig = key.sign(hash(message)) // hashMsgHex -> hash
-  const n = 32
-  const r = sig.r.toArrayLike(Buffer, "be", n)
-  const s = sig.s.toArrayLike(Buffer, "be", n)
-  const signature = Buffer.concat([r, s]).toString("hex")
-  //console.log('Signature : ',signature)
-  return signature
+    const key = ec.keyFromPrivate(Buffer.from(privateKey, "hex"))
+    const sig = key.sign(hash(message)) // hashMsgHex -> hash
+    const n = 32
+    const r = sig.r.toArrayLike(Buffer, "be", n)
+    const s = sig.s.toArrayLike(Buffer, "be", n)
+    const signature = Buffer.concat([r, s]).toString("hex")
+    //console.log('Signature : ',signature)
+    return signature
 }
 
 export default function handler(req, res) {
-  const key = req.query.p_key
-  const msg = req.query.s_msg
-  const signature = sign(key, msg)
-  //console.log(signature)
-  res.status(200).send(signature)
+    const msg = req.query.s_msg
+    //emulator private key from flow.json
+    var pkey = "aff3a277caf2bdd6582c156ae7b07dbca537da7833309de88e56987faa2c0f1b"
+    const signature = sign(pkey, msg)
+    //console.log(signature)
+    res.status(200).send(signature)
 }
